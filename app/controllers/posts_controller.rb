b1932de69f88635f2119409bfc03e543a188578c
Matthew Saunders <matthew.saunders12@gmail.com>
2017-02-02 12:02:12 -0500

    Add create to controller and basic index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,28 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.all
+  end
+
   def new
     @post = Post.new
   end
 
   def create
+    if Post.create(post_params)
+      redirect_to posts_path
+    else
+      render "new"
+    end
   end
-  
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :link)
+    end
+
+    def find_post
+      @post = Post.find(params[:id])
+    end
+
 end
