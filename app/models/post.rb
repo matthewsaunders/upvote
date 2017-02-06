25dde8ad6a41c700e179fbc26ee81bbf574ba06a
Matthew Saunders <matthew.saunders12@gmail.com>
2017-02-05 23:29:19 -0500

    Refactor posts index page into divs and style

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
 
   validates_format_of :link, with: /\Ahttps?:\/\/w{3}?.?[^\.$.?#]*.[\a-zA-Z]*\z/
+
+  def host
+    uri = URI(link)
+    uri.host.blank? ? "" : uri.host
+  end
+
 end
