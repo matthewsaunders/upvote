c332d50fc4bce15a95222883633d27ab42c6fb98
Matthew Saunders <matthew.saunders12@gmail.com>
2017-02-09 10:43:10 -0500

    Allow users to add comments to posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,30 @@
 class CommentsController < ApplicationController
 
+  before_filter :find_post, only: [:index, :create]
+
   def index
     @comment = Comment.new
     @comments = Comment.where(post_id: params[:post_id])
   end
+
+  def create
+    comment = Comment.new(comment_params)
+    comment.post_id = params[:post_id]
+    comment.user = current_user
+
+    if comment.save
+      redirect_to post_comments_path(params[:post_id])
+    else
+      render "index"
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
+
+    def find_post
+      @post = Post.find(params[:post_id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :posts do
-    resources :comments, only: [:index]
+    resources :comments, only: [:index, :create]
   end
 
   root to: 'posts#index'
